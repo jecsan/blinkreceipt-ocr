7fdab9c9d4c54e574c131bc82d8eab5a133d8dad
Jose S <joeds@sourcepad.com>
2023-04-05 00:34:15 +0800

    Add custom view

--- app/src/main/java/com/blinkreceipt/ocr/MainViewModel.kt
+++ app/src/main/java/com/blinkreceipt/ocr/MainViewModel.kt
@@ -1,10 +1,37 @@
 package com.blinkreceipt.ocr
 
 import androidx.lifecycle.ViewModel
+import com.blinkreceipt.ocr.scanresult.BrScanResults
 import com.microblink.FrameCharacteristics
 import com.microblink.ScanOptions
+import com.microblink.core.ScanResults
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+
+
 
 class MainViewModel : ViewModel() {
+
+    private val confidenceThreshold = 90f
+
+    private val _scanResults = MutableStateFlow<ScanUiState?>(null)
+    val scanResults: StateFlow<ScanUiState?> = _scanResults
+
+
+    private fun isPassableReceipt(scanResults: ScanResults): Boolean {
+        return scanResults.ocrConfidence() > confidenceThreshold
+    }
+
+    fun setScanResults(brScanResults: BrScanResults) {
+
+        if (isPassableReceipt(brScanResults.scanResults)) {
+            _scanResults.value = ScanUiState.Success(brScanResults)
+        } else {
+            _scanResults.value = ScanUiState.Error("The receipt you provided is not valid")
+        }
+
+    }
 
     fun generateScanOptions(): ScanOptions {
         return ScanOptions.newBuilder()
@@ -17,5 +44,6 @@ class MainViewModel : ViewModel() {
             .logoDetection(true)
             .build()
     }
+
 
 }
